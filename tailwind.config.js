3555454802e4949a7a0423d8f056543bc7bf2b37
Mohammad Jahid <mohammadjahid0007@gmail.com>
2022-05-11 22:30:20 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+
+  doctorThemes: [
+    {
+      mytheme: {
+        primary: "#0FCFEC",
+        secondary: "#19D3AE",
+        accent: "#3A4256",
+        neutral: "#3d4451",
+        "base-100": "#ffffff",
+      },
+    },
+    "dark",
+    "cupcake",
+  ],
+  plugins: [require("daisyui")],
+}
